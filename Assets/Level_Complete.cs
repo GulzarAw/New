a5ff828bc6ec45e8c1cf10ca2292882a8da458c0
GulzarAw <gulzar.awais@plomx.com>
2024-05-10 15:33:55 +0500

    .

--- Assets/Level_Complete.cs
+++ Assets/Level_Complete.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class Level_Complete : MonoBehaviour
 {
     public GameObject All_Char;
+    public GameObject next_Button;
     // Start is called before the first frame update
     void Start()
     {
         All_Char = GameObject.FindGameObjectWithTag("Games");
+    }
+    private void OnEnable()
+    {
+        if(PlayerData.SelectedLevelID == 9)
+        {
+            next_Button.SetActive(false);
+        }
+        else
+        {
+            next_Button.SetActive(true);
+        }
     }
 
     // Update is called once per frame
